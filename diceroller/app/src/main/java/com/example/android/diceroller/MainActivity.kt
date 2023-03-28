219d87ac7188b4195aefb7bf65fb4fa533f5b626
Pauló Lajos <dev@paulolajos.hu>
2023-03-28 06:00:54 +0200

    14-18. Connecting to button, change the text

--- diceroller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ diceroller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.android.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.android.diceroller.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Creating a binding object for the main_activity.xml layout
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        //Get a reference to the root view by either calling the getRoot() method
+        // or using Kotlin property syntax.
+        val view = binding.root
+
+        //Pass the root view to setContentView() to make it the active view on the screen.
+        setContentView(view)
+
+        binding.btRoll.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val resultText: TextView = binding.tvNumber
+        resultText.text = randomInt.toString()
     }
 }
