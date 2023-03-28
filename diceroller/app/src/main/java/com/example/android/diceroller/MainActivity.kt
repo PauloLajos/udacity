b47c4470a1c98b84ec416ba473f05adb26268a8d
Pauló Lajos <dev@paulolajos.hu>
2023-03-28 06:24:00 +0200

    19-21. Dice images, adding resources, ImageView

--- diceroller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ diceroller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.android.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.ImageView
 import com.example.android.diceroller.databinding.ActivityMainBinding
 import java.util.*
 
@@ -28,7 +28,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-        val resultText: TextView = binding.tvNumber
-        resultText.text = randomInt.toString()
+        val diceImage: ImageView = binding.ivDiceImage
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
