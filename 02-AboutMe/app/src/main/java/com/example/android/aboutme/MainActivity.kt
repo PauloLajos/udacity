eeb7cbee54b0fef63a3a360a6772f461f582ba01
Pauló Lajos <dev@paulolajos.hu>
2023-03-31 08:16:19 +0200

    11-13. Layouts - ...Add EditText, Done Button, ClickHandler, hide keyboard

--- 02-AboutMe/app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ 02-AboutMe/app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.android.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.example.android.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Creating a binding object for the main_activity.xml layout
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        //Get a reference to the root view by either calling the getRoot() method
+        // or using Kotlin property syntax.
+        val view = binding.root
+
+        //Pass the root view to setContentView() to make it the active view on the screen.
+        setContentView(view)
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        binding.nicknameText.text = binding.nicknameEdit.text
+        binding.nicknameEdit.visibility = View.GONE
+        view.visibility = View.GONE
+        binding.nicknameText.visibility = View.VISIBLE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
